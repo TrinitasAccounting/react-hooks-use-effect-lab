c837383d165a623c9819ed066ae9890b90c83062
Clay Mangum <clay@trinitasaccounting.com>
2024-02-23 19:49:03 -0500

    do not have time to be stuck on a lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+
+  // useEffect(() => {
+  //   //   setInterval(() => {
+  //   //     return setTimeRemaining(timeRemaining - 1)
+  //   //   }, 1000)
+  //   // }, [])
+  //   setTimeout(() => {
+  //     setTimeRemaining(timeRemaining - 1)
+  //   }, 1000)
+  // }, [])
+
+
+  useEffect(() => {
+
+    if (timeRemaining > 0) {
+      const intervalTest = setInterval(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+    }
+    else if (timeRemaining === 0) {
+      clearInterval(intervalTest);
+      setTimeRemaining(10)
+    }
+
+
+
+
+
+
+    // const intervalTest = setInterval(() => {
+    //   setTimeRemaining(timeRemaining - 1);
+    // }, 1000);
+
+    // return () => clearInterval(intervalTest);
+  }, [timeRemaining])
+
+
+
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
